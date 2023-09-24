156ce3ceb41891a507182abcbb0e2e251391e66d
st0p-01 <137320379+st0p-01@users.noreply.github.com>
2023-09-24 19:07:53 -0300

    .

---
+++ exercicios/ex003.py
@@ -0,0 +1,9 @@
+n1 = input("Digite um número: ")
+n2 = input("Digite um número: ")
+
+if n1 > n2:
+    print(f"O PRIMEIRO valor é maior")
+elif n1 < n2:
+    print(f"O SEGUNDO valor é maior")
+else:
+    print(f"Não existe um valor maior ou menor , os dois são IGUAIS!")
